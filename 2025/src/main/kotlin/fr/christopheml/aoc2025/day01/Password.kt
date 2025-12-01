504c4b35e9894cbee1486eac939bb069220b2848
Christophe Michel <1050366+christopheml@users.noreply.github.com>
2025-12-01 11:29:40 +0100

    :sparkles: Adds the solution for 2025 day 01

---
+++ 2025/src/main/kotlin/fr/christopheml/aoc2025/day01/Password.kt
@@ -0,0 +1,34 @@
+package fr.christopheml.aoc2025.day01
+
+import fr.christopheml.aoc.common.Input
+import fr.christopheml.aoc.common.multiLine
+import fr.christopheml.aoc.common.runner.Solution
+
+class Password : Solution<Input.MultiLine, Int>(
+  day = 1,
+  inputReader = ::multiLine
+) {
+
+  override fun partOne(input: Input.MultiLine): Int = rotate(input)
+    .count { it % 100 == 0 }
+
+  override fun partTwo(input: Input.MultiLine): Int = rotate(input)
+    .windowed(2, 1)
+    .sumOf { clicksOnZero(it[0], it[1]) }
+
+  private fun rotate(input: Input.MultiLine): Sequence<Int> = input.asSequence()
+    .map { it.toRotation() }
+    .scan(50, Int::plus)
+
+  private fun String.toRotation(): Int = this.drop(1).toInt() * if (this[0] == 'L') -1 else 1
+
+  private fun clicksOnZero(origin: Int, destination: Int): Int =
+    (if (origin < destination) origin..destination else origin downTo destination)
+      .drop(1)
+      .count { it % 100 == 0 }
+
+}
+
+fun main() {
+  Password().run()
+}
---
+++ 2025/src/test/kotlin/fr/christopheml/aoc2025/day01/PasswordTest.kt
@@ -0,0 +1,41 @@
+package fr.christopheml.aoc2025.day01
+
+import fr.christopheml.aoc.test.MultiLineAcceptanceTest
+import fr.christopheml.aoc.test.SingleLineAcceptanceTest
+import fr.christopheml.aoc.test.shouldBecome
+import org.junit.jupiter.api.Assertions.*
+
+class PasswordTest : MultiLineAcceptanceTest<Int>(::Password) {
+
+  override val forPartOne = listOf(
+    """
+      L68
+      L30
+      R48
+      L5
+      R60
+      L55
+      L1
+      L99
+      R14
+      L82
+    """.trimIndent() shouldBecome 3
+  )
+
+  override val forPartTwo = listOf(
+    """
+      R1000
+      L68
+      L30
+      R48
+      L5
+      R60
+      L55
+      L1
+      L99
+      R14
+      L82
+    """.trimIndent() shouldBecome 16
+  )
+
+}
